234d29e27de9087c4fd30dc5ca87d7da7544c317
Wish_Time <wish-time@outlook.com>
2017-12-20 11:39:45 +0800

    Initial commit

---
+++ templets/default/style/root.js
@@ -0,0 +1,8 @@
+﻿$(function() {
+    var b = $('.header .frame .box');
+    b.find('.nav').mouseenter(function() { $(this).css('height', 'auto'); b.animate({ height: '240px' }, 'fast'); });
+    b.mouseleave(function() { b.animate({ height: '92px' }, 'fast', function() { b.find('.nav').css('height', '92px'); }); });
+    $('.adplay').each(adSlide);
+    adScroll();
+});
+
